b667e9ce2e7cce0d122a7d3530a40d0e1e834577
Akaneyana <uwu.akane8@gmail.com>
2025-01-20 13:29:23 +0100

    First commit, basic file structure

---
+++ backend/database/tables.sql
@@ -0,0 +1,26 @@
+CREATE DATABASE IF NOT EXISTS TranslateService
+
+USE TranslateService
+
+CREATE TABLE Users (
+    User_id INT NOT NULL AUTO_INCREMENT,
+    FirstName VARCHAR(80) NOT NULL,
+    LastName VARCHAR(80) NOT NULL,
+    Password_hash VARCHAR(300) NOT NULL, 
+    Email VARCHAR(100) NOT NULL, 
+    CreationDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (User_id)
+);
+
+CREATE TABLE Orders (
+    Order_id INT NOT NULL AUTO_INCREMENT,
+    User_id INT NOT NULL,
+    Translation VARCHAR(100) NOT NULL,
+    OrderDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (Order_id)
+    FOREIGN KEY (User_id) REFERENCES Users(User_id)
+);
+
+SELECT Orders.Order_id, Users.FirstName, Orders.OrderDate
+FROM Orders
+INNER JOIN Customers ON Orders.User_id=Users.FirstName;
